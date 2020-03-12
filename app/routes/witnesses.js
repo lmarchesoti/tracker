93d0ab1d416ad6eb909e0b6c3d3a6241d4288e8b
Lucas Marchesoti Franco <lucas.marchesoti@callink.com.br>
2020-03-12 15:28:48 -0300

    Chapter 22

--- app/routes/witnesses.js
+++ app/routes/witnesses.js
@@ -2,11 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class WitnessesRoute extends Route {
   model() {
-    let witnessRecord = this.store.createRecord('witness', {
-      fName: "Todd",
-      lName: "Gandee",
-      email: "fake@bignerdranch.com"
-    });
-    return [witnessRecord];
+    return this.store.findAll('witness');
   }
 }
