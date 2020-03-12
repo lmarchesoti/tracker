7d369cdacca36d8c5f4531ed8d62ad9c2bbf7b30
Lucas Marchesoti Franco <lucas.marchesoti@callink.com.br>
2020-03-12 10:43:37 -0300

    Adds title with default values to witness

--- app/models/witness.js
+++ app/models/witness.js
@@ -5,8 +5,12 @@ export default class WitnessModel extends Model {
   @attr('string') fName;
   @attr('string') lName;
   @attr('string') email;
+  @attr('string', {defaultValue: 'Mr.'}) title;
   @hasMany('sighting') sightings;
   @computed('fName', 'lName', function () {
     return this.get('fName') + ' ' + this.get('lName');
   }) fullName;
+  @computed('title', 'lName', function () {
+    return this.get('title') + ' ' + this.get('lName');
+  }) titleName;
 }
--- app/routes/witnesses.js
+++ app/routes/witnesses.js
@@ -2,11 +2,23 @@ import Route from '@ember/routing/route';
 
 export default class WitnessesRoute extends Route {
   model() {
-    let witnessRecord = this.store.createRecord('witness', {
+    let witnessRecord1 = this.store.createRecord('witness', {
       fName: "Todd",
       lName: "Gandee",
-      email: "fake@bignerdranch.com"
+      email: "fake@bignerdranch.com",
+      title: "Mahatma"
     });
-    return [witnessRecord];
+    let witnessRecord2 = this.store.createRecord('witness', {
+      fName: "Lucas",
+      lName: "Marchesoti",
+      email: "lmarchesoti@gmail.com"
+    });
+    let witnessRecord3 = this.store.createRecord('witness', {
+      fName: "Ronne",
+      lName: "Scherhelton",
+      email: "ronne@callink.com",
+      title: "Master"
+    });
+    return [witnessRecord1, witnessRecord2, witnessRecord3];
   }
 }
