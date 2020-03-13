75e4b255375f083e5dcfe435a7c35c294f5b5af5
Lucas Marchesoti Franco <lucas.marchesoti@callink.com.br>
2020-03-13 10:37:58 -0300

    Adds ember-cli-content-security-policy
    Configuration does not work as shown in addon readme, it works in a variable defined in ENV at environment.js

--- config/environment.js
+++ config/environment.js
@@ -20,6 +20,16 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': ["'self'", "bnr-tracker-api.herokuapp.com"],
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': null
     }
   };
 
